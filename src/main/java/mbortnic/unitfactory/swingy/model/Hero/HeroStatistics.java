9efeaf58a6d676b5bf86713ab1bff99bb52143bf
Mariana BORTNICHUK <mbortnic@e1r6p14.unit.ua>
2019-02-01 18:47:31 +0200

    player and player statistics aaded

--- src/main/java/mbortnic/unitfactory/swingy/model/Hero/HeroStatistics.java
+++ src/main/java/mbortnic/unitfactory/swingy/model/Hero/HeroStatistics.java
@@ -1,4 +1,80 @@
 package mbortnic.unitfactory.swingy.model.Hero;
 
 public class HeroStatistics {
+
+    public HeroStatistics() {
+    }
+
+    private int exp;
+    private int lvl;
+    private int attack;
+    private int hitp;
+    private int protection;
+    private String playerType;
+    private int pow;
+
+    public HeroStatistics(String playerType, int lvl, int attack, int protection, int hitp, int exp) {
+        this.playerType = playerType;
+        this.lvl = lvl;
+        this.protection = protection;
+        this.hitp = hitp;
+        this.exp = exp;
+        this.attack = attack;
+        this.pow = attack + protection;
+    }
+
+    public String getPlayerType() {
+        return (this.playerType);
+    }
+
+    public int getExp() {
+        return (this.exp);
+    }
+
+    public int getLvl() {
+        return (this.lvl);
+    }
+
+    public int getAttack() {
+        return (this.attack);
+    }
+
+    public int getHitp() {
+        return (this.hitp);
+    }
+
+    public int getProtection() {
+        return (this.protection);
+    }
+
+    public int getPow() {
+        return (this.pow);
+    }
+
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public void setHitp(int hitp) {
+        this.hitp += hitp;
+
+        if (this.hitp <= 0) {
+            this.hitp = 0;
+        }
+    }
+
+    public void setProtection(int protection) {
+        this.protection += protection;
+
+        if (this.protection <= 0) {
+            this.protection = 0;
+        }
+    }
+
+
 }
--- src/main/java/mbortnic/unitfactory/swingy/model/Hero/Player.java
+++ src/main/java/mbortnic/unitfactory/swingy/model/Hero/Player.java
@@ -1,4 +1,45 @@
 package mbortnic.unitfactory.swingy.model.Hero;
 
+import com.sun.istack.internal.NotNull;
+import mbortnic.unitfactory.swingy.model.Artifact.Artifact;
+
 public class Player {
+
+    public Player() {
+
+    }
+
+    @NotNull
+    private Artifact art;
+
+    @NotNull
+    private HeroStatistics heroStatistics = new HeroStatistics();
+
+    @NotNull
+    private String newPlayer;
+
+
+    protected Player(String newPlayer, HeroStatistics heroStatistics, Artifact art) {
+        this.newPlayer = newPlayer;
+        this.heroStatistics = heroStatistics;
+        this.art = art;
+    }
+
+    public String getNewPlayer() {
+        return this.newPlayer;
+    }
+
+    public HeroStatistics getHeroStatistics() {
+        return this.heroStatistics = heroStatistics;
+    }
+
+    public Artifact getArt() {
+        return this.art = art;
+    }
+
+    public void setArt(Artifact art) {
+        this.art = art;
+    }
+
+
 }
