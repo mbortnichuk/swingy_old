2d619c460bb87d72c21d283f93c93ecf228943b4
Mariana BORTNICHUK <mbortnic@e1r5p16.unit.ua>
2019-02-09 21:45:15 +0200

    start function in console mode is done

--- src/main/java/mbortnic/unitfactory/swingy/view/console/Console.java
+++ src/main/java/mbortnic/unitfactory/swingy/view/console/Console.java
@@ -1,6 +1,10 @@
 package mbortnic.unitfactory.swingy.view.console;
 
 import mbortnic.unitfactory.swingy.model.Hero.Player;
+import mbortnic.unitfactory.swingy.reader.ReadFromFile;
+import mbortnic.unitfactory.swingy.view.gui.SwingyView;
+
+import java.util.Scanner;
 
 /**
  * Created by mbortnic on 2/9/19.
@@ -16,10 +20,57 @@ public class Console {
         int createPlayer;
 
         try {
-            
+            createPlayer = DisplayPlayer.setUpHero();
+            if (createPlayer == 1) {
+                hero = DisplayPlayer.greetPlayer();
+                type = DisplayPlayer.printHeroList();
+                player = SwingyView.determinePlayer(hero, type);
+                start = ShowStatistics.showInfo(type, hero, player);
+
+                if (start == 1) {
+//                    GameControll.go(player);
+                } else {
+                    System.out.println("Thanks for visiting my game!\n");
+                    System.exit(0);
+                }
+            } else if (createPlayer == 2) {
+                ReadFromFile.getBaseOfPlayers();
+
+                Scanner scanner = new Scanner(System.in);
+
+                while (scanner.hasNextLine()) {
+                    String str = scanner.nextLine();
+                    int linesNbr = ReadFromFile.getLinesInFile();
+                    if (isNbr(str) == true) {
+                        try {
+                            int ind = Integer.parseInt(str);
+                            if (ind > 0 && ind <= linesNbr) {
+                                option = ind;
+                                break ;
+                            }
+                        } catch (Exception exception) {
+                            System.out.println("Try one more time.");
+                        }
+                    } else {
+                        System.out.println("Try one more time.");
+                    }
+                    player = SwingyView.DBPlayer(ReadFromFile.getPlayer(option));
+//                    GameControll.go(player);
+                }
+            }
         } catch (Exception exception) {
             exception.getMessage();
         }
     }
+
+    public static boolean isNbr(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c) != true) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
